cc09eb28e0141311a009d48381d796be98c3f82a
Izmaylova <Natalyatf@yandex.ru>
2022-11-19 21:55:20 +0500

    Саисок продуктов: Домашнее задание 1: задание 1 Домашнее задание 2: задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Product potato = new Product("Картошка", 19, 0.8);
+        Product tomato = new Product("Помидоры", 129, 1.5);
+        Product cucumbers = new Product("Огурцы", 69, 1.1);
+        Product oil = new Product("Масло подсолнечное", 109, 0.5);
+        Product mayonnaise = new Product("Майонез", 159, 0.5);
+        Product greenery = new Product("Зелень", 59, 0.1);
+
+        ProductService service = new ProductService();
+        service.add(potato);
+        service.add(tomato);
+        service.add(cucumbers);
+        service.add(oil);
+        service.add(mayonnaise);
+        service.add(greenery);
+        System.out.println(service);
+
+
+        System.out.println("Список продуктов: " + service.getProducts());
+
+        service.delete(mayonnaise);
+
+        System.out.println("Список продуктов: " + service.getProducts());
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
